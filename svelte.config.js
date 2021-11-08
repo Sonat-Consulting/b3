d9510b0519842f3e3dfbdff3e9e4de59e539314f
Jonas Lundeland <jlundela@gmail.com>
2021-11-08 20:34:17 +0100

    Add netlify configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from "@sveltejs/adapter-static"
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,10 +10,17 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
+			pages: 'build',
+			assets: 'build',
 			fallback: null
-		})
+		}),
+
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'continue',
+			entries: ['*']
+		}
 	}
 };
 

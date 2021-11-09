014beaeea285d0d761abf388c26fb3ea9c017fe7
Jonas Lundeland <jlundela@gmail.com>
2021-11-09 18:09:07 +0100

    Add svelte head on each page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>B3 | About</title>
+</svelte:head>
+
 <h1>About</h1>
 
 <p>This is the about page</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,25 @@
 <script context="module">
-    export async function load({fetch}) {
-        const res = await fetch('/contentful.json')
+	export async function load({ fetch }) {
+		const res = await fetch('/contentful.json');
 
-        if (res.ok) {
-            const {homePage} = await res.json()
-            return {
-                props: {homePage}
-            }
-        }
-    }
+		if (res.ok) {
+			const { homePage } = await res.json();
+			return {
+				props: { homePage }
+			};
+		}
+	}
 </script>
 
 <script>
-    export let homePage
+	export let homePage;
 </script>
 
+<svelte:head>
+	<title>B3</title>
+</svelte:head>
+
 <div class="banner">
-    <h1>{homePage.topBannerTitle}</h1>
-    <img alt={homePage.topBannerImage.description} src={homePage.topBannerImage.url}/>
+	<h1>{homePage.topBannerTitle}</h1>
+	<img alt={homePage.topBannerImage.description} src={homePage.topBannerImage.url} />
 </div>

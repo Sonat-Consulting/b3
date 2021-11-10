5e897187c3aed66d1d5025bd574dad464054bc45
Jonas Lundeland <jlundela@gmail.com>
2021-11-10 07:10:49 +0100

    Add ArticleLink component

---
+++ src/lib/article/link.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let url: string;
+	export let date: string;
+	export let imageSrc: string;
+	export let imageAltText: string;
+	export let title: string;
+	export let ingress: string;
+</script>
+
+<a class="p-2" href={url}>
+	<div class="relative">
+		<img src={imageSrc} alt={imageAltText} class="rounded-lg shadow-sm w-full h-52 object-cover" />
+		<div
+			class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
+		>
+			{date}
+		</div>
+	</div>
+
+	<div class="my-2 text-xl font-semibold">{title}</div>
+	<div class="">{ingress}</div>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<script context="module" lang="ts">
 	export async function load({ fetch }) {
 		const res = await fetch('/contentful.json');
 
@@ -11,8 +11,9 @@
 	}
 </script>
 
-<script>
+<script lang="ts">
 	import Card from '$lib/card.svelte';
+	import ArticleLink from '$lib/article/link.svelte';
 	export let homePage;
 </script>
 
@@ -39,55 +40,30 @@
 </Card>
 
 <div class="container p-6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
-	<a class="p-2" href="/">
-		<div class="relative">
-			<img
-				src="https://akamai.vgc.no/v2/images/88ef682e-2407-4f21-a386-999dab0df0ac?fit=crop&h=1267&w=1900&s=99b26e158ae4acd38034ce517f8d5a68a0232c8e"
-				alt="Emoji Support"
-				class="rounded-lg shadow-sm w-full h-52 object-cover"
-			/>
-			<div
-				class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
-			>
-				Jul 15, 22
-			</div>
-		</div>
+	<ArticleLink
+		imageAltText="Beskrivelse av bilde"
+		url="/"
+		date="Jul 15, 22"
+		imageSrc="https://akamai.vgc.no/v2/images/88ef682e-2407-4f21-a386-999dab0df0ac?fit=crop&h=1267&w=1900&s=99b26e158ae4acd38034ce517f8d5a68a0232c8e"
+		title="Folkefest"
+		ingress="Bli med og arranger årets folkefest. Vi trenger frivillige."
+	/>
 
-		<div class="my-2 text-xl font-semibold">Folkefest</div>
-		<div class="">Dette blir moro</div>
-	</a>
-	<a class="p-2" href="/">
-		<div class="relative">
-			<img
-				src="https://g.acdn.no/obscura/API/dynamic/r1/ece5/tr_480_320_l_f/0000/berg/2018/12/6/17/BA73958.jpeg?chk=FA7818"
-				alt="Math Typesetting"
-				class="rounded-lg shadow-sm w-full h-52 object-cover"
-			/>
-			<div
-				class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
-			>
-				Jul 14, 22
-			</div>
-		</div>
+	<ArticleLink
+		imageAltText="Beskrivelse av bilde"
+		url="/"
+		date="Jul 14, 22"
+		imageSrc="https://g.acdn.no/obscura/API/dynamic/r1/ece5/tr_480_320_l_f/0000/berg/2018/12/6/17/BA73958.jpeg?chk=FA7818"
+		title="Sykkel"
+		ingress="Noen mener at sykling kan være gøy. Hva mener du?"
+	/>
 
-		<div class="my-2 text-xl font-semibold">Sykkel</div>
-		<div class="">Sykling kan være gøy</div>
-	</a>
-	<a class="p-2" href="/">
-		<div class="relative">
-			<img
-				src="https://media.snl.no/media/137414/standard_compressed_19UWjWe_p2g.jpg"
-				alt="Markdown Syntax Guide"
-				class="rounded-lg shadow-sm w-full h-52 object-cover"
-			/>
-			<div
-				class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
-			>
-				Jul 14, 22
-			</div>
-		</div>
-
-		<div class="my-2 text-xl font-semibold">Svømming</div>
-		<div class="">Her er noe beskrivende tekst om svømming</div>
-	</a>
+	<ArticleLink
+		imageAltText="Beskrivelse av bilde"
+		url="/"
+		date="Jul 13, 22"
+		imageSrc="https://media.snl.no/media/137414/standard_compressed_19UWjWe_p2g.jpg"
+		title="Svømming"
+		ingress="Andre mener svømming kan være gøy. Ihvertfall om man kan svømme."
+	/>
 </div>

f15b52f2e5c128d15d6f55a780e41c2196807b40
Jonas Lundeland <jlundela@gmail.com>
2022-01-09 19:16:26 +0100

    Add button styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,8 +45,10 @@
 </Card>
 
 <div class="relative h-12 text-center">
-	<div class="absolute mt-6 border-t-2 border-sky-300 w-full" />
-	<div class="btn-secondary mx-auto relative z-10 h-full bg-white">
+	<div class="absolute mt-6 border-t-2 border-sky-300 w-full z-0" />
+	<div
+		class="font-medium border-2 border-sky-300 inline-block rounded-xl text-center px-8 py-2.5 h-full bg-white relative z-10"
+	>
 		Siste nyheter fra Bergen Triathlon Events
 	</div>
 </div>

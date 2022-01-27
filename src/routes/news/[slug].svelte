73881467a9b6c1a2e82209ea1c351aaae432f676
Jonas Lundeland <jlundela@gmail.com>
2022-01-27 23:08:30 +0100

    Render rich text article body

--- src/routes/news/[slug].svelte
+++ src/routes/news/[slug].svelte
@@ -13,16 +13,24 @@
 </script>
 
 <script lang="ts">
+	import { BLOCKS } from '@contentful/rich-text-types';
+	import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 	import News from '$lib/news.svelte';
 	export let article;
 	export let articles;
+
+	const richTextParserOpts = {
+		renderNode: {
+			[BLOCKS.PARAGRAPH]: (node, next) => `<p class="mb-6">${next(node.content)}</p>`
+		}
+	};
 </script>
 
 <svelte:head>
 	<title>B3</title>
 </svelte:head>
 
-<div class="container flex flex-col mx-auto">
+<div class="container flex flex-col mx-auto article">
 	<div
 		class="container max-w-screen-md mx-auto pt-6 md:pt-12 pb-6 px-6 mb-3 flex items-center flex-col"
 	>
@@ -36,20 +44,7 @@
 			src={article.articleHeroImage.url}
 		/>
 
-		<p>
-			Norway’s Kristian Blummenfelt underlined his status as the greatest triathlete on the planet
-			right now with a consummate performance in Edmonton on Saturday afternoon to take both the
-			gold and the 2021 world title, the first man to do so in the same year as winning the Olympic
-			Games. Marten Van Riel (BEL) had piled on the pressure thanks to a great three-way break on
-			the bike along with 2020 champion Vincent Luis and Hungary’s Mark Devay, but Blummenfelt had
-			driven on the large chasing train for much of the 40km ride to ensure he was still well in
-			touch out onto the 10km run. With his main challenger for the title Alex Yee a good 90 seconds
-			back off the bike, the Norwegian stayed patient for 8km then timed his surge to perfection,
-			just as he had in Tokyo, Van Riel and Leo Bergere (FRA) ensuring a grandstand finish as the
-			three summoned an almighty sprint for the line, Blummenfelt roaring in celebration, the
-			Belgian taking silver by a whisker. That also meant the Series silver for Van Riel, while Yee
-			was able to run his way into 11th in the race and overall bronze.
-		</p>
+		{@html documentToHtmlString(article.body.json, richTextParserOpts)}
 	</div>
 
 	<News {articles} />

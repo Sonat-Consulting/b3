5d91dc656335614f985859f7b121033961983f5c
Jonas Lundeland <jlundela@gmail.com>
2022-02-11 17:58:01 +0100

    Add b3 footer

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,1 +1,106 @@
-<footer />
+<footer class="footer-1 bg-[#333333] pt-8 pb-8 md:pt-16 text-white px-4">
+	<div class="container mx-auto">
+		<!-- Visible on sm screen only -->
+		<div class="w-full md:hidden items-start items-center flex flex-col mb-10 md:mb-0">
+			<p class="text-center mb-4">World Triathlon Cup Bergen</p>
+			<p class="text-4xl mb-8">28. august 2022</p>
+			<a class="btn-primary text-black" href="https://bte.mobilise.no/Applicant.aspx?arr=9082758"
+				>Meld deg som frivillig</a
+			>
+		</div>
+		<div class="flex sm:-mx-4">
+			<div class="w-1/2 md:w-1/4 sm:px-4">
+				<ul class="list-none footer-links">
+					<li class="mb-4">
+						<a
+							href="https://bte.mobilise.no/Applicant.aspx?arr=9082758"
+							class="border-b-2 border-solid border-sky-300 hover:border-white pb-1">Frivillige</a
+						>
+					</li>
+					<li class="mb-4">
+						<a
+							href="/news/triathlon%20verdenscup%20l%C3%B8ype%20bergen"
+							class="border-b-2 border-solid border-sky-300 hover:border-white pb-1">Løypen</a
+						>
+					</li>
+				</ul>
+			</div>
+
+			<div class="hidden md:flex md:w-2/4 items-start items-center flex-col -mt-8">
+				<p class="text-center mb-4">World Triathlon Cup Bergen</p>
+				<p class="text-4xl mb-8">28. august 2022</p>
+				<a class="btn-primary text-black" href="https://bte.mobilise.no/Applicant.aspx?arr=9082758"
+					>Meld deg som frivillig</a
+				>
+			</div>
+
+			<div class="w-1/2 md:w-1/4 sm:px-4">
+				<div class="mb-5 flex flex-row items-center justify-end">
+					<a href="https://www.instagram.com/worldtriathlon">
+						<svg
+							class="inline-block"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<g clip-path="url(#clip0_499_60)">
+								<path
+									d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z"
+									stroke="#56CCF2"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M15.9997 11.3698C16.1231 12.2021 15.981 13.052 15.5935 13.7988C15.206 14.5456 14.5929 15.1512 13.8413 15.5295C13.0898 15.9077 12.2382 16.0394 11.4075 15.9057C10.5768 15.7721 9.80947 15.3799 9.21455 14.785C8.61962 14.1901 8.22744 13.4227 8.09377 12.592C7.96011 11.7614 8.09177 10.9097 8.47003 10.1582C8.84829 9.40667 9.45389 8.79355 10.2007 8.40605C10.9475 8.01856 11.7975 7.8764 12.6297 7.99981C13.4786 8.1257 14.2646 8.52128 14.8714 9.12812C15.4782 9.73496 15.8738 10.5209 15.9997 11.3698Z"
+									stroke="#56CCF2"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</g>
+							<defs>
+								<clipPath id="clip0_499_60">
+									<rect width="24" height="24" fill="white" />
+								</clipPath>
+							</defs>
+						</svg>
+						<span class="ml-2 border-b-2 border-solid border-sky-300 hover:border-white pb-1"
+							>Instagram</span
+						>
+					</a>
+				</div>
+				<div class="flex flex-row items-center justify-end">
+					<a href="https://www.facebook.com/Bergen-Triatlon-Events-131515645776593">
+						<svg
+							class="inline-block"
+							width="24"
+							height="24"
+							viewBox="0 0 13 22"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M12 1H9C7.67392 1 6.40215 1.52678 5.46447 2.46447C4.52678 3.40215 4 4.67392 4 6V9H1V13H4V21H8V13H11L12 9H8V6C8 5.73478 8.10536 5.48043 8.29289 5.29289C8.48043 5.10536 8.73478 5 9 5H12V1Z"
+								stroke="#56CCF2"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<span class="ml-2 border-b-2 border-solid border-sky-300 hover:border-white pb-1"
+							>Facebook</span
+						>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="mt-6 pt-6 sm:px-0 sm:mt-8 sm:pt-8 border-t border-sky-300">
+			<h6 class="font-bold mb-2">Bergen Triathlon Events AS</h6>
+			<p class="text-sm">Org nr. 925 926 477</p>
+		</div>
+	</div>
+</footer>

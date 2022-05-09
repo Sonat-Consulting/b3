3bca589a01c6baffdb9323d0c7397abd652722de
Jonas Lundeland <jlundela@gmail.com>
2022-05-09 12:59:10 +0200

    Add header menu placeholder content

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,20 +1,30 @@
-<header class="bg-white px-4 py-3 flex items-center justify-between">
-    <div class="flex items-center">
-        <a href="/" class="flex flex-row items-center">
-            <div class="whitespace-pre uppercase text-right">
-                World<br/>Triathlon<br/>Cup Bergen
-            </div>
-            <div class="px-2 ml-4 h-full">
-                <img alt="World triathlon cup Bergen 2022" src="/2022.svg"/>
-            </div>
-        </a>
+<header class="bg-white">
+    <div class="px-4 py-3 flex items-center justify-between">
+        <div class="flex items-center">
+            <a href="/" class="flex flex-row items-center">
+                <div class="whitespace-pre uppercase text-right">
+                    World<br/>Triathlon<br/>Cup Bergen
+                </div>
+                <div class="px-2 ml-4 h-full">
+                    <img alt="World triathlon cup Bergen 2022" src="/2022.svg"/>
+                </div>
+            </a>
+        </div>
+
+        <div>
+            <button type="button" class="block hover:text-gray-500">
+                <svg class="h-7 fill-current" viewBox="0 0 18 12">
+                    <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z"/>
+                </svg>
+            </button>
+        </div>
     </div>
 
-    <div>
-        <button type="button" class="block hover:text-gray-500">
-            <svg class="h-7 fill-current" viewBox="0 0 18 12">
-                <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" />
-            </svg>
-        </button>
+    <div class="pl-2 pr-4 pt-2 pb-5">
+        <a class="block text-lg px-2 mb-1 font-semibold rounded hover:bg-sky-300" href="#">Go to this</a>
+        <a class="block text-lg px-2 mb-1 font-semibold rounded hover:bg-sky-300" href="#">Go to that</a>
+        <a class="block text-lg px-2 mb-1 font-semibold rounded hover:bg-sky-300" href="#">Trips</a>
+        <a class="block text-lg px-2 font-semibold rounded hover:bg-sky-300" href="#">Athletes</a>
     </div>
+
 </header>

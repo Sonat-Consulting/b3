320db14ff77c9bcb256186209f02244d669823a3
Jonas Lundeland <jlundela@gmail.com>
2022-05-09 15:34:17 +0200

    Display menu links from contentful CMS

---
+++ src/routes/contentful/menu.json.ts
@@ -0,0 +1,24 @@
+import type {EndpointOutput} from '@sveltejs/kit/types/endpoint';
+import {gql} from 'graphql-request';
+import graphQLClient from '$lib/contentful/client';
+import type {MenuItem, MenuItemCollectionResponse} from "$lib/contentful/types/MenuItemCollectionResponse";
+
+export const get = async (): Promise<EndpointOutput<MenuItem[]>> => {
+    const query = gql`
+		query GetMenuItems {
+          menuLinksCollection(limit: 10) {
+            items {
+              url
+              text
+            }
+          }
+        }
+	`;
+
+    const response = await graphQLClient.request<MenuItemCollectionResponse>(query);
+
+    return {
+        status: 200,
+        body: response.menuLinksCollection.items
+    };
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,53 +1,62 @@
 <script context="module" lang="ts">
-	import { HomePage } from '$lib/contentful/types/HomePage';
+    import {HomePage} from '$lib/contentful/types/HomePage';
 
-	export async function load({ fetch }): Promise<any> {
-		const [homePageRes, articlesRes] = await Promise.all([
-			fetch('/contentful/home.json'),
-			fetch('/contentful/articles.json')
-		]);
+    export async function load({fetch}): Promise<any> {
+        const [homePageRes, articlesRes, menuRes] = await Promise.all([
+            fetch('/contentful/home.json'),
+            fetch('/contentful/articles.json'),
+            fetch('/contentful/menu.json')
+        ]);
 
-		if (homePageRes.ok) {
-			const homePage: HomePage = await homePageRes.json();
-			const articles = await articlesRes.json();
-			return { props: { homePage, articles } };
-		}
-	}
+        if (homePageRes.ok) {
+            const homePage: HomePage = await homePageRes.json();
+            const articles = await articlesRes.json();
+            const menuItems = await menuRes.json();
+            return {props: {menuItems, homePage, articles}};
+        }
+    }
 </script>
 
 <script lang="ts">
-	import Hero from '$lib/hero.svelte';
-	import Card from '$lib/card.svelte';
-	import News from '$lib/news.svelte';
-	import type { Article } from '$lib/contentful/types/Article';
-	export let homePage: HomePage;
-	export let articles: Article[];
+    import Header from '$lib/header.svelte';
+    import Hero from '$lib/hero.svelte';
+    import Card from '$lib/card.svelte';
+    import News from '$lib/news.svelte';
+    import type {Article} from '$lib/contentful/types/Article';
+    import type {MenuItem} from "$lib/contentful/types/MenuItemCollectionResponse";
+
+    export let menuItems: MenuItem[] = [];
+    export let homePage: HomePage;
+    export let articles: Article[];
 </script>
 
 <svelte:head>
-	<title>B3</title>
+    <title>B3 </title>
 </svelte:head>
 
-<Hero />
+<Header menuItems={menuItems}/>
+
+<Hero/>
 
 <div class="container flex flex-col mx-auto">
-	<Card>
-		<div>
-			<h1 class="text-4xl font-bold mb-4">{homePage.content.topBannerTitle}</h1>
-			<p class="font-light text-lg mb-8">
-				{homePage.content.topBannerSubtitle}
-			</p>
-			<div class="card-actions">
-				<a class="btn-primary mb-8 mr-8" href="https://bte.mobilise.no/Applicant.aspx?arr=9082758">Meld deg som frivillig</a>
-				<a class="btn-secondary mb-8 mr-8" href="/news/frivillig">Mer informasjon</a>
-			</div>
-		</div>
-		<img
-			class="rounded-lg shadow-sm col-span-2"
-			alt={homePage.content.topBannerImage.description}
-			src={homePage.content.topBannerImage.url}
-		/>
-	</Card>
+    <Card>
+        <div>
+            <h1 class="text-4xl font-bold mb-4">{homePage.content.topBannerTitle}</h1>
+            <p class="font-light text-lg mb-8">
+                {homePage.content.topBannerSubtitle}
+            </p>
+            <div class="card-actions">
+                <a class="btn-primary mb-8 mr-8" href="https://bte.mobilise.no/Applicant.aspx?arr=9082758">Meld deg som
+                    frivillig</a>
+                <a class="btn-secondary mb-8 mr-8" href="/news/frivillig">Mer informasjon</a>
+            </div>
+        </div>
+        <img
+                class="rounded-lg shadow-sm col-span-2"
+                alt={homePage.content.topBannerImage.description}
+                src={homePage.content.topBannerImage.url}
+        />
+    </Card>
 
-	<News {articles} />
+    <News {articles}/>
 </div>

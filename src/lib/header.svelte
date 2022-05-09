fe4e16056a5cf0b1dd307d362461a8887118c3dc
Jonas Lundeland <jlundela@gmail.com>
2022-05-09 12:45:08 +0200

    Add hamburger menu icon to the the header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,20 +1,20 @@
-<header class="px-6 py-4">
-	<nav class="flex items-center justify-between bg-none text-none-content">
-		<div class="flex-1 flex items-center">
-			<a href="/" class="flex flex-row items-center">
-				<div class="whitespace-pre uppercase text-right">
-					World<br />Triathlon<br />Cup Bergen
-				</div>
-				<div class="px-2 ml-4 w-24 md:w-auto hidden md:block">
-					<img alt="World triathlon cup Bergen 2022" src="/2022.svg" />
-				</div>
-			</a>
-		</div>
+<header class="bg-white px-4 py-3 flex items-center justify-between">
+    <div class="flex items-center">
+        <a href="/" class="flex flex-row items-center">
+            <div class="whitespace-pre uppercase text-right">
+                World<br/>Triathlon<br/>Cup Bergen
+            </div>
+            <div class="px-2 ml-4 h-full">
+                <img alt="World triathlon cup Bergen 2022" src="/2022.svg"/>
+            </div>
+        </a>
+    </div>
 
-		<div class="flex-none">
-			<div class="flex items-stretch">
-				<a class="btn-primary" href="https://bte.mobilise.no/Applicant.aspx?arr=9082758">Meld deg som frivillig</a>
-			</div>
-		</div>
-	</nav>
+    <div>
+        <button type="button" class="block hover:text-gray-500">
+            <svg class="h-7 fill-current" viewBox="0 0 18 12">
+                <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" />
+            </svg>
+        </button>
+    </div>
 </header>

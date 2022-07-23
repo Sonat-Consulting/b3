14e98c11a64841a37bdc9b0d2863db6f9e8fb827
Jonas Lundeland <jlundela@gmail.com>
2022-07-23 22:35:09 +0200

    Display months in countdown component

--- src/lib/countDown.svelte
+++ src/lib/countDown.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { isAfter, intervalToDuration, Interval } from 'date-fns';
 
-	const plannedStartTime = new Date('2022-08-28T07:00:00Z');
+	let timeoutRef: number, hasStarted: boolean, timeToPlannedStart: Interval;
 
-	let hasStarted;
-	let timeToPlannedStart: Interval;
+	const TICK_MS = 1001;
+	const plannedStartTime = new Date('2022-08-28T07:00:00Z');
 
 	(function tick() {
 		const now = new Date();
 		timeToPlannedStart = intervalToDuration({ start: now, end: plannedStartTime });
 		hasStarted = isAfter(now, plannedStartTime);
-		setTimeout(tick, 1000);
+		timeoutRef = setTimeout(tick, TICK_MS) as number;
 	})();
+
+	onDestroy(() => {
+		clearTimeout(timeoutRef);
+	});
 </script>
 
 {#if !hasStarted}
@@ -20,9 +25,19 @@
 			<div class="grid grid-flow-col gap-5 text-center text-black auto-cols-max">
 				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
 					<span class="countdown font-mono text-black text-4xl">
+						<span style="--value:{timeToPlannedStart.months};" />
+					</span>
+					{#if timeToPlannedStart.months === 1}
+						måned
+					{:else}
+						måneder
+					{/if}
+				</div>
+				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
+					<span class="countdown font-mono text-black text-4xl">
 						<span style="--value:{timeToPlannedStart.days};" />
 					</span>
-					{#if timeToPlannedStart.days == 1}
+					{#if timeToPlannedStart.days === 1}
 						dag
 					{:else}
 						dager
@@ -32,7 +47,7 @@
 					<span class="countdown font-mono text-4xl">
 						<span style="--value:{timeToPlannedStart.hours};" />
 					</span>
-					{#if timeToPlannedStart.hours == 1}
+					{#if timeToPlannedStart.hours === 1}
 						time
 					{:else}
 						timer

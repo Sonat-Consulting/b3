c4c6a103314c7d33caf2b9e91efce95e10f2e130
Jonas Lundeland <jlundela@gmail.com>
2022-07-23 23:04:18 +0200

    Responsive countdown component

--- src/lib/countDown.svelte
+++ src/lib/countDown.svelte
@@ -20,51 +20,49 @@
 </script>
 
 {#if !hasStarted}
-	<div class="absolute top-0 left-0 object-cover h-full w-full">
-		<div class="absolute right-5 top-5">
-			<div class="grid grid-flow-col gap-5 text-center text-black auto-cols-max">
-				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
-					<span class="countdown font-mono text-black text-4xl">
-						<span style="--value:{timeToPlannedStart.months};" />
-					</span>
-					{#if timeToPlannedStart.months === 1}
-						måned
-					{:else}
-						måneder
-					{/if}
-				</div>
-				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
-					<span class="countdown font-mono text-black text-4xl">
-						<span style="--value:{timeToPlannedStart.days};" />
-					</span>
-					{#if timeToPlannedStart.days === 1}
-						dag
-					{:else}
-						dager
-					{/if}
-				</div>
-				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
-					<span class="countdown font-mono text-4xl">
-						<span style="--value:{timeToPlannedStart.hours};" />
-					</span>
-					{#if timeToPlannedStart.hours === 1}
-						time
-					{:else}
-						timer
-					{/if}
-				</div>
-				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
-					<span class="countdown font-mono text-4xl">
-						<span style="--value:{timeToPlannedStart.minutes};" />
-					</span>
-					minutt
-				</div>
-				<div class="flex flex-col p-2 bg-white/50 rounded-box items-center">
-					<span class="countdown font-mono text-4xl">
-						<span style="--value:{timeToPlannedStart.seconds};" />
-					</span>
-					sekund
-				</div>
+	<div class="absolute right-3 top-3 md:right-5 md:top-5">
+		<div class="grid grid-flow-col gap-2 md:gap-4 text-center text-black auto-cols-max">
+			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+				<span class="countdown font-mono text-black text-xl md:text-3xl">
+					<span style="--value:{timeToPlannedStart.months};" />
+				</span>
+				{#if timeToPlannedStart.months === 1}
+					måned
+				{:else}
+					måneder
+				{/if}
+			</div>
+			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+				<span class="countdown font-mono text-black text-xl md:text-3xl">
+					<span style="--value:{timeToPlannedStart.days};" />
+				</span>
+				{#if timeToPlannedStart.days === 1}
+					dag
+				{:else}
+					dager
+				{/if}
+			</div>
+			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+				<span class="countdown font-mono text-xl md:text-3xl">
+					<span style="--value:{timeToPlannedStart.hours};" />
+				</span>
+				{#if timeToPlannedStart.hours === 1}
+					time
+				{:else}
+					timer
+				{/if}
+			</div>
+			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+				<span class="countdown font-mono text-xl md:text-3xl">
+					<span style="--value:{timeToPlannedStart.minutes};" />
+				</span>
+				minutt
+			</div>
+			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+				<span class="countdown font-mono text-xl md:text-3xl">
+					<span style="--value:{timeToPlannedStart.seconds};" />
+				</span>
+				sekund
 			</div>
 		</div>
 	</div>

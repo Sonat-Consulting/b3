4dde14af6061a2e3c03a1605d2bcf1977a6abbaf
Jonas Lundeland <jlundela@gmail.com>
2022-07-27 23:01:11 +0200

    Display countdown months and days if value gt zero

--- src/lib/countDown.svelte
+++ src/lib/countDown.svelte
@@ -22,26 +22,32 @@
 {#if !hasStarted}
 	<div class="absolute right-3 top-3 md:right-5 md:top-5">
 		<div class="grid grid-flow-col gap-2 md:gap-4 text-center text-black auto-cols-max">
-			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
-				<span class="countdown font-mono text-black text-xl md:text-3xl">
-					<span style="--value:{timeToPlannedStart.months};" />
-				</span>
-				{#if timeToPlannedStart.months === 1}
-					måned
-				{:else}
-					måneder
-				{/if}
-			</div>
-			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
-				<span class="countdown font-mono text-black text-xl md:text-3xl">
-					<span style="--value:{timeToPlannedStart.days};" />
-				</span>
-				{#if timeToPlannedStart.days === 1}
-					dag
-				{:else}
-					dager
-				{/if}
-			</div>
+			{#if timeToPlannedStart.months > 0}
+				<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+					<span class="countdown font-mono text-black text-xl md:text-3xl">
+						<span style="--value:{timeToPlannedStart.months};" />
+					</span>
+					{#if timeToPlannedStart.months === 1}
+						måned
+					{:else}
+						måneder
+					{/if}
+				</div>
+			{/if}
+
+			{#if timeToPlannedStart.days > 0}
+				<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
+					<span class="countdown font-mono text-black text-xl md:text-3xl">
+						<span style="--value:{timeToPlannedStart.days};" />
+					</span>
+					{#if timeToPlannedStart.days === 1}
+						dag
+					{:else}
+						dager
+					{/if}
+				</div>
+			{/if}
+
 			<div class="flex flex-col p-2 md:p-3 bg-white/50 rounded-box items-center">
 				<span class="countdown font-mono text-xl md:text-3xl">
 					<span style="--value:{timeToPlannedStart.hours};" />

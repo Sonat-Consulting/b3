5f2eb6b8f8529b8b4dba5c4694deb8338ee77987
Jonas Lundeland <jlundela@gmail.com>
2022-08-29 17:25:11 +0200

    chore: replace hero video with webp image for faster load time

--- src/lib/hero.svelte
+++ src/lib/hero.svelte
@@ -2,17 +2,9 @@
 	import CountDown from '$lib/countDown.svelte';
 </script>
 
-<div class="w-full aspect-video relative overflow-hidden h-96 xl:h-112">
-	<video
-		playsinline
-		preload="auto"
-		autoplay
-		loop
-		muted
-		class="absolute top-0 left-0 object-cover h-full w-full"
-		poster="/rundemannen_poster.jpg"
-	>
-		<source src="/rundemannen_large.mp4" type="video/mp4" />
-	</video>
+<div
+	class="w-full aspect-video relative overflow-hidden h-96 xl:h-112 bg-cover"
+	style="background-image: url('/cover.webp')"
+>
 	<CountDown />
 </div>

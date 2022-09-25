326b060cc5eafe99ca4bc68c58043bed3c5ce8c3
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-25 19:10:44 +0200

    fix: add spacing between news links. Fix news date format

--- src/lib/article/link.svelte
+++ src/lib/article/link.svelte
@@ -9,14 +9,14 @@
 
 <a href={url}>
 	<div class="relative">
-		<img src={imageSrc} alt={imageAltText} class="rounded-lg shadow-sm w-full h-52 object-cover" />
+		<img src={imageSrc} alt={imageAltText} class="rounded-lg shadow-sm w-full object-cover" />
 		<div
-			class="absolute top-4 right-4 rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
+			class="absolute top-4 right-4 font-semibold rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
 		>
 			{date}
 		</div>
 	</div>
 
-	<div class="my-2 text-xl font-semibold">{title}</div>
-	<div class="">{ingress}</div>
+	<div class="mt-6 mb-4 text-xl font-semibold">{title}</div>
+	<div class="leading-relaxed">{ingress}</div>
 </a>

88696be777c1fb6e73d2fe4b940cff844f2b35ff
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-25 19:21:03 +0200

    fix: add background to page divider & place news date bottom right

--- src/lib/article/link.svelte
+++ src/lib/article/link.svelte
@@ -11,7 +11,7 @@
 	<div class="relative">
 		<img src={imageSrc} alt={imageAltText} class="rounded-lg shadow-sm w-full object-cover" />
 		<div
-			class="absolute top-4 right-4 font-semibold rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
+			class="absolute bottom-4 right-4 font-semibold rounded shadow bg-white text-gray-900 dark:bg-gray-900 dark:text-white text-sm px-2 py-0.5"
 		>
 			{date}
 		</div>

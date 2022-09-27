aeedec58b68b04612a23e2f6d4ba04cb6553eb47
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-09-27 20:15:07 +0200

    chore: refactor ArticleLink and News component

--- src/lib/components/News.svelte
+++ src/lib/components/News.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
 	import ArticleLink from '$lib/components/ArticleLink.svelte';
-	import { format, parseISO } from 'date-fns/index.js';
-	import { nb } from 'date-fns/locale/index.js';
 	import type { Article } from '$lib/types/b3';
 	export let articles: Article[];
 </script>
 
 <div class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-8 md:px-0">
 	{#each articles as article (article.slug)}
-		<ArticleLink
-			imageAltText={article.articleHeroImage?.title}
-			url={`/news/${article.slug}`}
-			date={article.date ? format(parseISO(article.date), 'd. MMMM yyyy', { locale: nb }) : ''}
-			imageSrc={`${article.articleHeroImage?.url}?fm=webp&w=720&h=480`}
-			title={article.title}
-			ingress={article.ingress}
-		/>
+		<ArticleLink {article} />
 	{/each}
 </div>

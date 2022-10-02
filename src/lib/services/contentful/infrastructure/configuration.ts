1916425b01a6b6725aefa3cee5cf6f65988d27d3
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-02 11:53:14 +0200

    chore: refactor rich text parser

--- src/lib/services/contentful/infrastructure/configuration.ts
+++ src/lib/services/contentful/infrastructure/configuration.ts
@@ -1,1 +1,3 @@
 export const isPreviewMode = import.meta.env.VITE_CONTENTFUL_PREVIEW == 'enabled';
+export const accessToken = import.meta.env.VITE_CONTENTFUL_DELIVERY_API_ACCESS_TOKEN;
+export const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;

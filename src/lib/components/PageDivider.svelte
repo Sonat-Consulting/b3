c92b73f8045863d4bbc5062a4dea4efc49139530
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-17 21:12:18 +0200

    chore: design updates to videos

--- src/lib/components/PageDivider.svelte
+++ src/lib/components/PageDivider.svelte
@@ -1,3 +1,3 @@
-<div class="relative text-center my-8 md:my-12">
+<div class="relative text-center my-16">
 	<div class="absolute h-0.5 bg-[#D5E6FB] w-full z-0" />
 </div>
--- src/lib/components/TopBanner.svelte
+++ src/lib/components/TopBanner.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div
-	class="max-w-screen-2xl m-auto grid grid-cols-12 grid-rows-6 h-112 lg:h-128 bg-cover bg-center"
+	class="max-w-screen-2xl m-auto grid grid-cols-12 grid-rows-6 h-128 lg:h-144 bg-cover bg-center"
 	style={`background-image: url(${topBanner.image.url})`}
 >
 	<div
-		class="bg-[#2F80ED]/80 col-span-12 row-span-6 md:row-span-6 md:col-start-9 md:col-span-4 text-white flex flex-col justify-center p-8"
+		class="bg-[#2F80ED]/80 col-span-12 row-start-4 row-span-3 md:row-span-6 md:col-start-9 md:col-span-4 text-white flex flex-col justify-center p-8"
 	>
 		<h1 class="text-xl font-bold mb-4">{topBanner.title}</h1>
 		<p class="font-light text-lg">

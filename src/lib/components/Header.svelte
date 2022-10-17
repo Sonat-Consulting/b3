f4eb7a59bff6a62a0e49d08b85435826b668187e
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-17 19:21:01 +0200

    chore: reduce header height and include bte logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,16 +5,15 @@
 	export let menuItems: MenuItem[] = [];
 </script>
 
-<header class="bg-gray-100">
+<header>
 	<div class="max-w-screen-2xl m-auto">
 		<div class="py-4 px-8 flex items-center justify-between">
 			<div class="flex items-center">
-				<a href="/" class="flex flex-row items-center">
-					<div class="flex flex-col text-right uppercase font-bold text-base">
-						<span>World</span>
-						<span>Triathlon</span>
-						<span>Cup Bergen</span>
-					</div>
+				<a href="/" class="flex flex-row items-center" title="Gå til forsiden">
+					<img
+						class="h-10"
+						src="https://images.ctfassets.net/wi3d49ltoppx/5Zbi1sNCknc8P5EQwPywXa/e544ebc2ff7d64bcc6e30247358b2729/bte.jpeg?fm=webp"
+					/>
 				</a>
 			</div>
 
@@ -35,7 +34,7 @@
 						on:click={() => (isOpen = !isOpen)}
 						class="block hover:text-gray-500"
 					>
-						<svg class:hidden={isOpen} class="h-7 fill-current" viewBox="0 0 122.88 95.95">
+						<svg class:hidden={isOpen} class="h-4 fill-current" viewBox="0 0 122.88 95.95">
 							<g>
 								<path
 									class="st0"
@@ -43,7 +42,7 @@
 								/>
 							</g>
 						</svg>
-						<svg class:hidden={!isOpen} class="h-7 fill-current" viewBox="0 0 122.878 122.88">
+						<svg class:hidden={!isOpen} class="h-4 fill-current" viewBox="0 0 122.878 122.88">
 							<g>
 								<path
 									d="M1.426,8.313c-1.901-1.901-1.901-4.984,0-6.886c1.901-1.902,4.984-1.902,6.886,0l53.127,53.127l53.127-53.127 c1.901-1.902,4.984-1.902,6.887,0c1.901,1.901,1.901,4.985,0,6.886L68.324,61.439l53.128,53.128c1.901,1.901,1.901,4.984,0,6.886 c-1.902,1.902-4.985,1.902-6.887,0L61.438,68.326L8.312,121.453c-1.901,1.902-4.984,1.902-6.886,0 c-1.901-1.901-1.901-4.984,0-6.886l53.127-53.128L1.426,8.313L1.426,8.313z"

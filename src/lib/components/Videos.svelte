3c5dc3a93ec5cbb804b4fa49396386331e3bb5e6
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-17 21:12:18 +0200

    chore: design updates to videos

--- src/lib/components/Videos.svelte
+++ src/lib/components/Videos.svelte
@@ -2,22 +2,29 @@
 	import type { Video } from '$lib/types/viewmodels';
 
 	export let videos: Video[] = [];
+
+	$: video = videos[0];
 </script>
 
-<section class="grid grid-cols-1 md:grid-cols-2 gap-20 px-8 md:px-0">
-	{#each videos as video (video.id)}
-		<div>
-			<div style="padding:56.25% 0 0 0" class="relative">
-				<video poster={video.poster} controls class="absolute top-0 left-0 w-full h-full">
-					{#each video.sources as source (source.uri)}
-						<source src={source.uri} type={source.type} />
-					{/each}
-				</video>
-			</div>
+<section class="px-8 md:px-0">
+	<div class="grid grid-cols-12 grid-rows-6 gap-x-8">
+		<div
+			class="row-start-5 row-span-2 lg:row-span-6 col-span-12 lg:col-span-3 flex flex-col justify-center"
+		>
 			{#if video.title && video.description}
-				<h2 class="mt-6 mb-4 text-xl font-semibold">{video.title}</h2>
+				<h2 class="mb-4 text-xl font-semibold">{video.title}</h2>
 				<p class="leading-relaxed">{video.description}</p>
 			{/if}
 		</div>
-	{/each}
+		<div
+			style="padding:56.25% 0 0 0"
+			class="relative row-span-4 lg:row-span-6 col-span-12 lg:col-span-9 lg:col-start-4"
+		>
+			<video poster={video.poster} controls class="absolute top-0 left-0 w-full h-full">
+				{#each video.sources as source (source.uri)}
+					<source src={source.uri} type={source.type} />
+				{/each}
+			</video>
+		</div>
+	</div>
 </section>

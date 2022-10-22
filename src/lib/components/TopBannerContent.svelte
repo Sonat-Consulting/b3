97639697fabdd547724b49491ab1f34d27314dc4
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-22 14:44:29 +0200

    chore: style top banner responsive

---
+++ src/lib/components/TopBannerContent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { TopBanner } from '$lib/types/viewmodels';
+
+	export let topBanner: TopBanner;
+</script>
+
+<h1 class="text-xl font-bold mb-4">{topBanner.title}</h1>
+<p class="font-light text-lg">
+	{topBanner.subtitle}
+</p>
+{#if topBanner.primaryLinkUrl || topBanner.secondaryLinkUrl}
+	<div class="card-actions mt-8">
+		{#if topBanner.primaryLinkUrl}
+			<a class="btn btn-primary" href={topBanner.primaryLinkUrl}>{topBanner.primaryLinkText}</a>
+		{/if}
+		{#if topBanner.secondaryLinkUrl}
+			<a class="btn btn-secondary" href={topBanner.secondaryLinkUrl}
+				>{topBanner.secondaryLinkText}</a
+			>
+		{/if}
+	</div>
+{/if}
--- src/lib/components/Videos.svelte
+++ src/lib/components/Videos.svelte
@@ -8,7 +8,7 @@
 
 <div class="grid grid-cols-12 grid-rows-6 gap-x-8">
 	<div
-		class="p-8 row-start-5 row-span-2 lg:row-span-6 col-span-12 lg:col-span-3 flex flex-col justify-center"
+		class="p-8 row-start-5 row-span-2 lg:row-span-6 col-span-12 lg:col-span-4 flex flex-col justify-center"
 	>
 		{#if video.title && video.description}
 			<h2 class="mb-4 text-xl font-semibold">{video.title}</h2>
@@ -16,13 +16,14 @@
 		{/if}
 	</div>
 	<div
-		style="padding:56.25% 0 0 0"
-		class="relative row-span-4 lg:row-span-6 col-span-12 lg:col-span-9 lg:col-start-4 bg-black"
+		class="row-span-4 lg:row-span-6 col-span-12 lg:col-span-8 lg:col-start-5 flex justify-center"
 	>
-		<video poster={video.poster} controls class="absolute top-0 left-0 w-full h-full">
-			{#each video.sources as source (source.uri)}
-				<source src={source.uri} type={source.type} />
-			{/each}
-		</video>
+		<div style="padding:56.25% 0 0 0" class="relative flex-1 bg-black">
+			<video poster={video.poster} controls class="absolute top-0 left-0 w-full h-full">
+				{#each video.sources as source (source.uri)}
+					<source src={source.uri} type={source.type} />
+				{/each}
+			</video>
+		</div>
 	</div>
 </div>

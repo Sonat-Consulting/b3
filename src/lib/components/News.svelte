86d53aab5ea8be8cb89becfe9c00ce4d9c239710
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-22 16:11:56 +0200

    chore: increase page spacings

--- src/lib/components/News.svelte
+++ src/lib/components/News.svelte
@@ -4,7 +4,7 @@
 	export let articles: Article[];
 </script>
 
-<section class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+<section class="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
 	{#each articles as article (article.slug)}
 		<ArticleLink {article} />
 	{/each}

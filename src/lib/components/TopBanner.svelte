a1d0dbeaf5c29de720953389970b07a6dfd0c10e
Jonas Lundeland <jonas.lundeland@tv2.no>
2022-10-22 14:44:29 +0200

    chore: style top banner responsive

--- src/lib/components/TopBanner.svelte
+++ src/lib/components/TopBanner.svelte
@@ -1,33 +1,20 @@
 <script lang="ts">
 	import type { TopBanner } from '$lib/types/viewmodels';
+	import TopBannerContent from '$lib/components/TopBannerContent.svelte';
 
 	export let topBanner: TopBanner;
 </script>
 
 <div
-	class="max-w-screen-2xl m-auto grid grid-cols-12 grid-rows-6 h-128 lg:h-144 bg-cover bg-center"
+	class="max-w-screen-2xl m-auto grid grid-cols-12 grid-rows-6 h-64 md:h-104 bg-cover bg-center"
 	style={`background-image: url(${topBanner.image.url})`}
 >
 	<div
-		class="bg-[#2F80ED]/80 col-span-12 row-start-4 row-span-3 md:row-span-6 md:col-start-9 md:col-span-4 text-white flex flex-col justify-center p-8"
+		class="bg-[#467FD4]/80 col-span-12 row-start-4 row-span-3 md:row-span-6 md:col-start-8 md:col-span-5 lg:col-span-4 lg:col-start-9 text-white flex-col justify-center p-8 hidden md:flex"
 	>
-		<h1 class="text-xl font-bold mb-4">{topBanner.title}</h1>
-		<p class="font-light text-lg">
-			{topBanner.subtitle}
-		</p>
-		{#if topBanner.primaryLinkUrl || topBanner.secondaryLinkUrl}
-			<div class="card-actions">
-				{#if topBanner.primaryLinkUrl}
-					<a class="btn btn-primary mb-8 mr-8" href={topBanner.primaryLinkUrl}
-						>{topBanner.primaryLinkText}</a
-					>
-				{/if}
-				{#if topBanner.secondaryLinkUrl}
-					<a class="btn btn-secondary mb-8 mr-8" href={topBanner.secondaryLinkUrl}
-						>{topBanner.secondaryLinkText}</a
-					>
-				{/if}
-			</div>
-		{/if}
+		<TopBannerContent {topBanner} />
 	</div>
+</div>
+<div class="md:hidden p-8 bg-[#467FD4] text-white">
+	<TopBannerContent {topBanner} />
 </div>
